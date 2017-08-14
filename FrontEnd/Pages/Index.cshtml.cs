31b4ca748dddb96d8c5d67e1be41613addbe9868
Phillip Grujovski <phil.grujovski@hotmail.com>
2017-08-14 14:25:04 +1000

    Index page code behinde update

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using FrontEnd.Services;
+using ConferenceDTO;
 
 namespace FrontEnd.Pages
 {
@@ -22,5 +23,27 @@ namespace FrontEnd.Pages
         public IEnumerable<(int Offset, DayOfWeek? DayofWeek)> DayOffsets { get; set; }
 
         public int CurrentDayOffset { get; set; }
+
+        public async Task OnGetAsync(int day = 0)
+        {
+            CurrentDayOffset = day;
+
+            var sessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+            var endDate = sessions.Max(s => s.EndTime?.Date);
+
+            var numberOfDays = ((endDate - startDate)?.Days) + 1;
+
+            DayOffsets = Enumerable.Range(0, numberOfDays ?? 0)
+                .Select(offset => (offset, (startDate?.AddDays(offset))?.DayOfWeek));
+
+            var filterDate = startDate?.AddDays(day);
+
+            Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+                               .OrderBy(s => s.TrackId)
+                               .GroupBy(s => s.StartTime)
+                               .OrderBy(g => g.Key);
+        }
     }
 }
