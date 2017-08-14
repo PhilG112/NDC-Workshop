f01e38ff35f64ed229555d5912af988cd2ea6825
Phillip Grujovski <phil.grujovski@hotmail.com>
2017-08-14 13:59:25 +1000

    misc

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FrontEnd.Services;
 
 namespace FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        protected readonly IApiClient _apiClient;
 
+        public IndexModel(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
         }
+
+        public IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>> Sessions { get; set; }
+
+        public IEnumerable<(int Offset, DayOfWeek? DayofWeek)> DayOffsets { get; set; }
+
+        public int CurrentDayOffset { get; set; }
     }
 }
